a1b1b357a24f45205cd7f23826d5c97ec28fb08f
CombatPug <pe.ladar@icloud.com>
2025-02-18 07:12:31 +0100

    SHARD-1808: add additional AJV validation for joinData additionalProperties (#324)
    * add additional AJV validation for joinData additionalProperties
    
    * fixes to schema
    
    * fixes to schema and verify
    
    ---------
    
    Co-authored-by: Aniket Divekar <1047471+aniketdivekar@users.noreply.github.com>

---
+++ src/types/ajv/JoinAppData.ts
@@ -0,0 +1,58 @@
+import { schemaSign } from './SignSchema'
+import {AJVSchemaEnum} from "../enum/AJVSchemaEnum";
+import {addSchema} from "../../utils/serialization/SchemaHelpers";
+
+export const schemaStakeCert = {
+  type: 'object',
+  properties: {
+    nominator: { type: 'string' },
+    nominee: { type: 'string' },
+    stake: { type: 'string' }, // bigint as string
+    certExp: { type: 'number' },
+    signs: {
+      type: 'array',
+      items: schemaSign,
+    },
+    sign: schemaSign,
+  },
+  required: ['nominator', 'nominee', 'stake', 'certExp'],
+  additionalProperties: false,
+}
+
+export const schemaAdminCert = {
+  type: 'object',
+  properties: {
+    nominee: { type: 'string' },
+    certCreation: { type: 'number' },
+    certExp: { type: 'number' },
+    sign: schemaSign,
+    goldenTicket: { type: 'boolean' },
+  },
+  required: ['nominee', 'certCreation', 'certExp', 'sign'],
+  additionalProperties: false,
+}
+
+export const schemaAppJoinData = {
+  type: 'object',
+  properties: {
+    version: { type: 'string' },
+    stakeCert: { anyOf: [schemaStakeCert, { type: 'null' }] },
+    adminCert: { anyOf: [schemaAdminCert, { type: 'null' }] },
+    isAdminCertUnexpired: { type: 'boolean' },
+  },
+  required: ['version'],
+  additionalProperties: false,
+}
+
+export function initJoinAppData(): void {
+  addSchemaDependencies()
+  addSchemas()
+}
+
+function addSchemaDependencies(): void {
+  // No dependencies
+}
+
+function addSchemas(): void {
+  addSchema(AJVSchemaEnum.AppJoinData, schemaAppJoinData)
+}
